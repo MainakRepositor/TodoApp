3b2842b5c0afe9d4bf48fc90c1e37e8f4a38467b
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2022-02-08 14:14:15 +0530

    Update settings.py

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
